187694a6fb6f74b33eb9e7695e141250dcf5b54e
ErickGuillen17 <80604246+ErickGuillen17@users.noreply.github.com>
2022-08-23 12:04:35 -0600

    Correción en campos de registro reclutador

--- BackEnd/Entities/usuarioReclutador.cs
+++ BackEnd/Entities/usuarioReclutador.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,20 @@ namespace BackEnd.Entities
     public partial class usuarioReclutador
     {
 
-       
 
+        [Display(Name = "Email")]
         public string CorreoReclutador { get; set; } = null!;
+        [Display(Name = "Nombre")]
         public string NombreReclutador { get; set; } = null!;
+        [Display(Name = "Apellido")]
         public string ApellidoReclutador { get; set; } = null!;
+        [Display(Name = "Compañía")]
         public string Compania { get; set; } = null!;
+        [Display(Name = "Teléfono")]
         public int TelefonoReclutador { get; set; }
-
+        [Display(Name = "Contraseña")]
         public string contrasena { get; set; }
+        [Display(Name = "Confirmación")]
         public string confirmacion { get; set; }
 
         public long idrol { get; set; }
